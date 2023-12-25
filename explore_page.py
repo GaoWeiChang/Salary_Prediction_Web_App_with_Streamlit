22d2028197c6c74cdd933d87652d13b79453f69a
Gao-Wei Chang <128176822+GaoWeiChang@users.noreply.github.com>
2023-12-25 20:24:31 +0800

    Update explore_page.py

--- explore_page.py
+++ explore_page.py
@@ -61,7 +61,7 @@ def show_explore_page():
     st.title("Explore Software Engineer Salaries")
     st.write(
         """
-    ### Stack Overflow Developer Survey 2020
+    ### Stack Overflow Developer Survey 2023
     """
     )
 
